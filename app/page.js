4587f74888e26fd50297f56cba5fb0918dea9a67
AsifKamalTias <asifkamaltias@gmail.com>
2024-06-14 12:29:31 +0600

    recent properties

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "./_components/Hero";
 import InfoBoxes from "./_components/InfoBoxes";
+import RecentProperties from "./_components/RecentProperties";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <RecentProperties />
     </>
   );
 }

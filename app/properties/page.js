68e066a44dc727fbc0092340382efbed7e8272d3
AsifKamalTias <asifkamaltias@gmail.com>
2024-06-14 12:02:21 +0600

    property listing

--- app/properties/page.js
+++ app/properties/page.js
@@ -1,7 +1,22 @@
-export default function Properties () {
-    return (
-        <main>
-            Properties
-        </main>
-    );
+import properties from "./../../properties.json";
+import Property from "../_components/Property";
+
+export default function Properties() {
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {
+          properties.length === 0 ? (
+            <p>No properties found.</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => {
+                return <Property key={property._id} property={property} />
+              })}
+            </div>
+          )
+        }
+      </div>
+    </section>
+  );
 }
